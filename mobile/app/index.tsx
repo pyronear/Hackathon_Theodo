629c9f003adb4fb085d9bbe079618e917119cd5a
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-09-03 10:40:20 +0200

    feat: add a message when all images where voted on

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,13 +1,30 @@
 import { fires } from '@/domain/fires';
 import { FireCard } from '@/view/FireCard';
+import { Text, View } from 'react-native';
 import { SwipeableCardStack } from 'react-native-swipeable-card-stack';
 
 export default function HomeScreen() {
   return (
-    <SwipeableCardStack
-      data={fires}
-      renderCard={FireCard}
-      lockedDirections={['bottom', 'top']}
-    />
+    <>
+      <View
+        style={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#cdaaf1',
+        }}
+      >
+        <Text style={{ fontSize: 22 }}>🔥🔥🔥</Text>
+      </View>
+      <SwipeableCardStack
+        data={fires}
+        renderCard={FireCard}
+        lockedDirections={['bottom', 'top']}
+      />
+    </>
   );
 }
