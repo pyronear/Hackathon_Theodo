346f05344c47e34b7c6b58daabe65036e603863e
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-09-03 10:24:18 +0200

    feat: add fires

---
+++ mobile/domain/fires.ts
@@ -0,0 +1,34 @@
+import { FireItem } from './FireItem';
+
+export const fires: FireItem[] = [
+  {
+    url: 'https://images.unsplash.com/photo-1624929303592-230462f729be?q=80&w=800',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1511027643875-5cbb0439c8f1?q=80&w=800',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1426170042593-200f250dfdaf?q=80&w=800',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1627404017595-212e8933788f?q=80&w=800',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1542202229-7d93c33f5d07?q=80&w=800',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1445964047600-cdbdb873673d?q=80&w=800',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1602980045360-d94be60e4775?q=80&w=800',
+  },
+  {
+    url: 'https://plus.unsplash.com/premium_photo-1675355675464-2deabb1f893b?q=80&w=800',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1602980068700-a090f985ded4?q=80&w=800',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1477862096227-3a1bb3b08330?q=80&w=800',
+  },
+];
