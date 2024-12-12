89954ecaecea15a045e62b6a434100553bbd026b
fe51 <55736935+fe51@users.noreply.github.com>
2024-12-12 17:09:58 +0100

    fix: Updated api server ip adress

--- mobile/src/api/images.queries.ts
+++ mobile/src/api/images.queries.ts
@@ -10,7 +10,7 @@ export const useImages = () => {
       for (let index = 0; index < 50; index++) {
         try {
           const data = await axios.get(
-            'http://141.94.127.211:8000/get_unlabeled_random_event',
+            'http://57.128.107.129:8000/get_unlabeled_random_event',
           );
 
           images.push({
@@ -42,7 +42,7 @@ export const useImages = () => {
 export const useSendFireResult = () => {
   const { mutate } = useMutation({
     mutationFn: async (data: { id: number; label: number }) => {
-      axios.post(`http://141.94.127.211:8000/labelize_event/${data.id}`, {
+      axios.post(`http://57.128.107.129:8000/labelize_event/${data.id}`, {
         label: data.label,
       });
     },
